1c273f051f6182e3c6ae81b2331aff528317f3ea
Kim <tnwndvhrrurr@paran.com>
2017-04-07 15:44:28 +0900

    Html Thymeleaf version

--- src/main/java/com/lpk/energy/DemoApplication.java
+++ src/main/java/com/lpk/energy/DemoApplication.java
@@ -2,10 +2,11 @@ package com.lpk.energy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
